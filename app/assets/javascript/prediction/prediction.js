cb513082c69a637148f36dc06de7c45d7fbf4ab8
ferbin <ferbin17@gmail.com>
2021-06-13 09:58:07 +0530

    feat: refactor user profile, changes/reset password

--- app/assets/javascript/prediction/prediction.js
+++ app/assets/javascript/prediction/prediction.js
@@ -1,6 +1,8 @@
 $(document).delegate("input.number_field", "keydown", function(e){
-  if((e.which >= 96 && e.which <= 105) || [8, 37, 38, 39, 40].includes(e.which)){
-    return true;
+  if((e.which >= 96 && e.which <= 105) || [8, 37, 38, 39, 40, 33].includes(e.which) || (e.which >= 48 && e.which <= 57)){
+    if((parseInt(String.fromCharCode(e.which)) != NaN) && !e.shiftKey){
+      return true;
+    }
   }
   return false;
 })
